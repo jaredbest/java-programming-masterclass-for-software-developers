1705ca6353b95196d5acafc22ae273a88cd9db5c
jaredbest <me@jaredbest.com>
2021-02-23 13:30:23 +0100

    Completed Section 3

---
+++ Section_03/FloatAndDouble/src/academy/learnprogramming/Main.java
@@ -0,0 +1,40 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f; // This format is preferred
+        // float myFloatValue = (float) 5.25; // This format is possible
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("myIntValue= " + myIntValue);
+        System.out.println("myFloatValue= " + myFloatValue);
+        System.out.println("myDoubleValue= " + myDoubleValue);
+
+        // A double is faster to process computationally than a float.
+        // Therefore, using a double over a float is recommended.
+        // Java treats double as the default floating-point number if the d or f is not specified.
+
+        // Challenge: Convert a given number of pounds to kilograms.
+
+        double numberOfPounds = 650d;
+        double convertedKilograms = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms= " + convertedKilograms);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+    }
+}
